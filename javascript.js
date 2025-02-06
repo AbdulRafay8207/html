6842eef3933bcbb06387ebd3fb7cd38c81295207
AbdulRafay8207 <abdulrafay.8207@gmail.com>
2025-02-06 11:53:52 +0500

    java

---
+++ javascript.js
@@ -0,0 +1,105 @@
+name = "Rafay"                                        
+age = 29,       
+pie = 3.147,    
+Pakistani = true
+Karachi = null  
+
+/* Non-primitive = Object */
+
+user1 = {
+name : "Rafay",                                      /* string data */
+age : 17,                                            /* Integer data */
+pie : 3.147,                                         /* float data */
+Pakistani : false,                                    /* Boolean data */
+Karachi : null                                      /* null/undefined data */      
+}
+
+user2 = {
+  name : "Abdul",
+  age : 20
+}
+
+/* Non-primitive = array */
+
+random = [2, 4, 6, 8]
+
+arr1 = [
+  user2 = {
+  name : "Abdul",
+  age : 20
+},
+
+user3 = {
+  name : "Rafay",
+  age : 17
+}
+]
+
+
+user4 = ["rafay", 2, true, 3, 4]
+
+
+var fname = "Abdul"
+  fname = "rafay"
+
+
+  obj1 = {
+    n : "rafay",
+    a : 17
+  }
+
+  obj2 = obj1
+
+  obj1 = {
+    n : "zameer",
+    a : 2
+  }
+  
+
+
+  
+a = 2
+b = a
+a = 4
+
+
+//Non-pirimitive data type with spread operator
+
+obj1 = {name : "Abdul", age : 17}
+obj2 = {...obj1}
+obj1.name = "Rafay"
+
+
+//Non-primitive data type with array
+
+obja = ["nameAbdul", "age17"]
+objb = obja
+obja = ["Rafay"]
+
+
+{
+  let a = 1
+  
+  {
+    
+    a =a+1
+    
+  }
+}
+
+{
+  const a = 2
+
+}
+
+ var a =  Date()
+ 
+ 
+
+ a = 2+3
+ s = 7-2
+ m = 2*2
+ d = 2/2
+ p = 2**3
+console.log(p);
+
