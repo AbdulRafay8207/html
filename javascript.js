f2383c6af7e9757c20df7359c1866979871924d8
AbdulRafay8207 <abdulrafay.8207@gmail.com>
2025-02-11 11:58:59 +0500

    more java

--- javascript.js
+++ javascript.js
@@ -73,8 +73,9 @@ obj1.name = "Rafay"
 //Non-primitive data type with array
 
 obja = ["nameAbdul", "age17"]
-objb = obja
+objb = {...obja}
 obja = ["Rafay"]
+
 
 
 {
@@ -101,5 +102,85 @@ obja = ["Rafay"]
  m = 2*2
  d = 2/2
  p = 2**3
-console.log(p);
 
+ fruits = ["apple", "banana", "cherry"]
+fruits.push(123,456)
+
+
+ fruits = ["apple", "banana", "cherry"]
+ lastfruit = fruits.pop()
+ 
+ //Concat
+
+ car = ["civic", "BMW"]
+ car2 = ["Honda"]
+ totalcars = car.concat(car2)
+ totalcars.push(123)
+ /*console.log(totalcars)
+ console.log(car)
+ console.log(car2)*/
+
+//include
+
+ fruits = ["apple", "banana", "cherry"]
+ fruit = fruits.includes("banana") //Answer is true
+
+ fruits = ["apple", "banana", "cherry"]
+ fruit = fruits.includes("mango") //Answer is false
+
+ //Array.isArray
+
+ fruits = ["apple", "banana", "cherry"]
+ fruit = Array.isArray(fruits)
+ 
+ a = [1]
+ b = Array.isArray(a)
+ 
+ //Slice
+
+ fruits = [1, 2, 3, 4, 5, 6,]
+ newfruit = fruits.slice(1, 3)
+//  console.log("sliceFruits", fruits)
+//  console.log("sliceNewfruit", newfruit)
+ 
+ //Splice
+
+ fruits = [1, 2, 3, 4, 5, 6,]
+ newitems = fruits.splice(1, 4)
+//  console.log("splice", fruits)
+//  console.log("splice", newitems)
+
+colorsetone = ["red", "blue"]
+colorsettwo = colorsetone 
+colorsetone.push("green")   //this is also known is shallow copy buz its not changing its refrence
+// console.log(colorsetone)
+// console.log(colorsettwo)
+
+ obj = {
+  name : "rafay",
+  name : "raFay"
+ }
+//  console.log(obj)
+
+ //       OBJECT EXERCISE #1
+
+ product = {
+  name : "HP ElitebookSleeve",
+  inStock : true,
+  price : 1000,
+  totalUnits : 7,
+  colors : ["black", "white", "gray"]
+ }
+  // console.log(product["name"])
+
+  // console.log(Math.random())
+
+  randomvalue = Math.random() * 100
+  // console.log(randomvalue)
+
+  randomenumber = {
+   name : "rafay",
+   address : Math.floor(Math.random()*7)
+  }
+  console.log(randomenumber)
+ 
