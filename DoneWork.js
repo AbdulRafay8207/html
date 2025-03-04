47ffe87191036ee6943eb64e29d31385df9508d9
AbdulRafay8207 <abdulrafay.8207@gmail.com>
2025-03-04 11:56:45 +0500

    ramadan

---
+++ DoneWork.js
@@ -0,0 +1,28 @@
+// Dom Exercise: 1
+// mytext = document.getElementById("text")
+// mytext.style.backgroundColor = "lightgray"
+// mytext.style.color = "green"
+// mytext.style.fontsize = "18px"
+
+// Dom Exercise: 2 ---------------------------------------------------------------------------
+
+// mytext = document.querySelector("paragraph")
+// testing = mytext.textContent = "New paragraph text"
+
+// myheading = document.querySelector("heading")
+// myheading.innerText = "New heading text"
+
+// Adding Elements to the DOM-----------------------------------------------------------------
+// subtitle = document.createElement("h2")
+// newtext = document.createTextNode("Baitussalam tech Park")
+// subtitle.appendChild(newtext)
+// document.body.appendChild(subtitle)
+
+// Dom Exercise:4------------------------------------------------------------------------------------
+
+// p = document.createElement("p")
+// tetxNode = document.createTextNode("hellostudents")
+// p.appendChild(textNode)
+// p.style.color = "green"
+// p.style.fontSize = "20px"
+// document.getElementById("content-section").appendChild(p)
