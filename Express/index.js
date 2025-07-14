b8f550a22bc4cb9bf3be8f3613ec43bb51a8f706
AbdulRafay8207 <abdulrafay.8207@gmail.com>
2025-07-15 00:55:32 +0500

    CRUD operation using express

--- Express/index.js
+++ Express/index.js
@@ -1,40 +1,61 @@
 const express = require('express')
 const app = express()
 
-// app.get('/:username', (req, res) => {
-//     // res.send(JSON.stringify({
-//     //     message: "Dashboard page" // Content type change
-//     // }))
-//     res.send({
-//         path: req.path,
-//         requObject: req.params,
-//         message: "Dashboard page"
-//     })
-// })
+let users = []
 
-app.use(()=>{
-    console.log('Inside use function');
-    
+app.use(express.json())
+
+app.post("/users", (req, res) => {
+    function create(obj) {
+        users.push(obj)
+    }
+    addUser = {
+        id: 1,
+        name: "abdul"
+    }
+    addUser2 = {
+        id: 2,
+        name: "rafay"
+    }
+    create(addUser)
+    create(addUser2)
+    res.send("User added")
 })
 
-function checkLogin(req, res, next){
-    console.log("Inside Login");
-    const login = true
-    if(!login) res.send("Please Login First")
-    next()
-}
-app.get('/account',checkLogin,(req, res)=>{
-    console.log("Account Page");
-    res.send("Account Page")
-    
+app.get("/users", (req, res) => {
+    res.send(users)
 })
 
+app.get("/users/:id", (req, res) => {
+    const userid = parseInt(req.params.id)
+    const user = users.find(user => user.id === userid)
+    if(!user){
+        return res.send("User not found")
+    }
+    res.send(user)
+})
 
+app.put("/users/:id", (req, res) => {
+    const userid = parseInt(req.params.id)
+    const user = users.find(user => user.id === userid)
+    if(!user){
+        return res.send("User not found")
+    }
+    user.name = req.body.name
+    res.send(user)
+})
 
-
-
+app.delete("/users/:id", (req, res) => {
+    const userid = parseInt(req.params.id)
+    const userIndex = users.findIndex(user => user.id === userid)
+    if(userIndex === -1){
+        return res.send("User not found")
+    }
+    deleteUser = users.splice(userIndex,1)
+    res.send("User Deleted")
+})
 
 const PORT = 3000
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("server started at", `http://localhost:${PORT}`);
 })
