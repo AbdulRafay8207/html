93c77983ed3cc559d3313c3a02071580673ba46d
HafizAbdulRafay <84220169+AbdulRafay8207@users.noreply.github.com>
2025-07-21 23:37:02 +0500

    Little change

--- StudentPortal/index.js
+++ StudentPortal/index.js
@@ -7,6 +7,24 @@ app.get("/login",(req,res) =>{
     res.render('htmlPages/login');
 })
 
+function createNewUser(req,res,next){
+    const rules = {
+        username: true,
+        password: true
+    }
+    const keys = Object.keys(rules)
+    const errors = {}
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+    }
+    
+    next()
+}
+
+app.post('/login',(req,res) => {
+
+})
+
 
 const PORT = 3000
 app.listen(PORT,()=> console.log("Server started at port",PORT))
